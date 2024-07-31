513b7e02c868d693f9dd247d3fe02e326748428a
Valorware Soft <valorwaresoftceo@gmail.com>
2024-08-01 01:22:30 +0530

    push

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FaqComponent } from "../../common-components/faq/faq.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FaqComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
