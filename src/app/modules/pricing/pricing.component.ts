5d91ee4eb59b7d35d2d5c4ab91b1486c066a51f8
Valorware Soft <valorwaresoftceo@gmail.com>
2024-08-13 02:47:23 +0530

    push

--- src/app/modules/pricing/pricing.component.ts
+++ src/app/modules/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, HostListener, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.scss']
 })
-export class PricingComponent {
+export class PricingComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
+  ngAfterViewInit() {
+    this.adjustStarsHeight();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.adjustStarsHeight();
+  }
+
+  adjustStarsHeight() {
+    const pricingDiv = this.el.nativeElement.querySelector('.service-div');
+    const starsContainer = this.el.nativeElement.querySelector('#CODEVEMBER');
+    if (pricingDiv && starsContainer) {
+      this.renderer.setStyle(starsContainer, 'height', `${pricingDiv.offsetHeight}px`);
+    }
+  }
 }
