f7a9945524cfefd10180b7d1547399eb9b206782
Sowjanyamutha <sowjanyalucky3@gmail.com>
2024-09-13 09:56:27 +0530

    1st push

--- src/app/common-components/privacy-policy/privacy-policy.component.ts
+++ src/app/common-components/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss']
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements AfterViewInit {
+  @ViewChild('scrollTarget', { static: false }) scrollTarget: ElementRef | undefined;
 
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    const scrollElement = this.elRef.nativeElement.querySelector('.scroll-target');
+    if (scrollElement) {
+      scrollElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
--- src/app/common-components/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/common-components/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-terms-and-conditions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './terms-and-conditions.component.html',
   styleUrls: ['./terms-and-conditions.component.scss']
 })
-export class TermsAndConditionsComponent {
+export class TermsAndConditionsComponent implements AfterViewInit {
+  @ViewChild('scrollTarget', { static: false }) scrollTarget: ElementRef | undefined;
 
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+   
+    const scrollElement = this.elRef.nativeElement.querySelector('.scroll-target');
+    if (scrollElement) {
+      scrollElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
